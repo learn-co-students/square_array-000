2a57e3378133a984652b4bdfc2070e7cd4ab5c90
mbrickenworthe <adamwinters456@gmail.com>
2015-06-22 18:21:19 -0400

    FInally

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,19 @@
-def square_array(array)
-  # your code here
+def square_array(givenArray)
+  newArray = Array.new
+  givenArray.each do |num| 
+  newNum = num * num
+  puts newNum
+  newArray.push(newNum)
+  end
+  return newArray
+end
+
+def square_array2(givenArray)
+  newArray2 = Array.new
+  givenArray.each do |num| 
+    squareNumber = num ** 2
+    puts squareNumber
+  newArray2.push(squareNumber)
+  return newArray2
+  end
 end
