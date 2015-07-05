08606c88fab2639553329c0b420a23f6af45d4f7
jonhunter13 <jonhunter13@gmail.com>
2015-07-05 15:45:25 -0400

    Done.

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,16 @@
 def square_array(array)
   # your code here
+  count = 0
+  array.each { |num|
+    array[count] = num * num
+    count +=1
+  }
+  array
 end
+
+def collect_arr(arr)
+  arr.collect{|num| num * num}
+end
+
+#collect_arr([1, 4, 9])
+#square_array([1, 4, 9])
